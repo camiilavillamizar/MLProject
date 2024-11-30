fe86b33e3d319c9cf472c8e615e9bfc8835f335f
Maria Camila Villamizar <camiilavillamizar@gmail.com>
2024-11-29 22:35:30 -0500

    Frontend finished

--- FrontEnd/src/app/components/scan/scan.component.ts
+++ FrontEnd/src/app/components/scan/scan.component.ts
@@ -11,7 +11,20 @@ import { NgxFileDropEntry } from 'ngx-file-drop';
 })
 export class ScanComponent {
 
+  state: string = "initial"; 
+  error: string = ""
   actualFile: File | undefined ;
+  diagnosis: string = ""; 
+  selectedImage: string | undefined; 
+
+  images: {[key: string]: string} = {
+    'parasites': 'parasite.jpg',
+    'infection': 'infection.jpeg',
+    'autoimmune': 'autoimmune.jpeg',
+    'healthy': 'dogs-2.jpeg', 
+    'allergy': 'allergy.jpg'
+
+  }
   constructor(private scanService: ScanService){}
 
   public files: NgxFileDropEntry[] = [];
@@ -29,6 +42,8 @@ export class ScanComponent {
       fileEntry.file((file: File) => {
         //Verify extension
         if (file.type === 'image/jpeg' || file.type === 'image/png') {
+          this.selectedImage = URL.createObjectURL(file);
+          this.state = "loading"
           this.actualFile = file; 
           console.log('Uploaded file:', file);
           this.getSkinDisease(file)
@@ -42,8 +57,10 @@ export class ScanComponent {
   onFileSelected(event: any){
     const file: File = event.target.files[0];
     if (file.type === 'image/jpeg' || file.type === 'image/png') {
+      this.state = "loading"
       this.actualFile = file
       console.log('Selected file:', file);
+      this.selectedImage = URL.createObjectURL(file);
       this.getSkinDisease(file)
     } else {
       alert('Only JPG or PNG files are allowed.');
@@ -54,10 +71,21 @@ export class ScanComponent {
     this.scanService.getDisease(file).subscribe(
       res => {
         console.log("Success! Disease: ", res)
+        this.state = "processed"
       }, 
       error => {
         console.log("Error "+ error)
+        this.state = "error"
+        this.error = error.error;
       }
     )
+  }
+
+  tryAgain(){
+    this.state = "initial"
+    this.error = ""
+    this.actualFile = undefined; 
+    this.diagnosis = ""; 
+    this.selectedImage = undefined; 
   }
 }
--- FrontEnd/src/app/components/scan/scan.module.ts
+++ FrontEnd/src/app/components/scan/scan.module.ts
@@ -5,10 +5,12 @@ import { ScanRoutingModule } from './scan-routing.module';
 import { ScanComponent } from './scan.component';
 import { MaterialModule } from '../../material/material.module';
 import { NgxFileDropModule } from 'ngx-file-drop';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [
-    ScanComponent
+    ScanComponent,
+    LoadingComponent
   ],
   imports: [
     CommonModule,
